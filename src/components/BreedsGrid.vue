fdeef1724d63d1cbf0d469b2e8bff423284ab488
tmilost <tmilos94@gmail.com>
2021-09-13 00:17:37 +0200

    create all breed grid

--- src/components/BreedsGrid.vue
+++ src/components/BreedsGrid.vue
@@ -1,103 +1,169 @@
 <template>
   <div class="hello">
     <div>
-      <div class="container">
-  <div class="photos">
-    <img src="http://fakeimg.pl/360x360/000/?text=1">
-    <img src="http://fakeimg.pl/360x360/111/?text=2">
-    <img src="http://fakeimg.pl/360x360/222/?text=3">
-    <img src="http://fakeimg.pl/360x360/333/?text=4">
-    <img src="http://fakeimg.pl/360x360/444/?text=5">
-    <img src="http://fakeimg.pl/360x360/555/?text=6">
-    <img src="http://fakeimg.pl/360x360/666/?text=7">
-    <img src="http://fakeimg.pl/360x360/777/?text=8">
-    <img src="http://fakeimg.pl/360x360/888/?text=9">
-    <img src="http://fakeimg.pl/360x360/999/?text=10">
-    <img src="http://fakeimg.pl/360x360/aaa/?text=11">
-    <img src="http://fakeimg.pl/360x360/bbb/?text=12">
-  </div>
-</div>
+      <div class="image-grid">
+        <div class="grid-image" v-for="(breed,index) in RandomImageBreeds" :key="breed">
+          <img
+            style="width: 250px; height: 170px"
+            class="centered project-image-bg"
+            :src="breed"
+            alt=""
+          />
+          <button
+            class="
+              uppercase
+              viu-button viu-button--secondary
+              viu-buttons-list__list-item
+            "
+          >
+           {{allBreeds[index]}} 
+          </button>
+        </div>
+      </div>
 
+      <!-- /////////////////////// -->
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import axios from "axios";
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 
-   mounted() {
-    //do something
+  private allBreeds: string[] = [];
+  private RandomImageBreeds: string[] = [];
+
+  public async getAllBreeds() {
+    const { data } = await axios.get("https://dog.ceo/api/breeds/list/all");
+
+    for (let breed in data.message) {
+      this.allBreeds.push(breed);
+    }
+	this.getRandomImageBreed()
+    ////////////////////////////////
+ 
+  }
+
+  public async getRandomImageBreed() {
+      this.allBreeds.forEach((breed) => {
+      axios
+        .get("https://dog.ceo/api/breed/" + breed + "/images/random")
+        .then((response) => {
+          this.RandomImageBreeds.push(response.data.message);
+  
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  }
+
+  //  public getRandomImageBreed() {
+  //  var ss:string;
+  //   axios.get("https://dog.ceo/api/breed/hound/images/random").then((response) => {
+  //       ss = response.data.message;
+  //         console.log(response.data.message);
+
+  //       })
+  //       .catch((e) => {
+  //         console.log(e);
+  //       });
+  //  return "https://images.dog.ceo/breeds/hound-afghan/n02088094_5345.jpg";
+
+  //   }
+
+  mounted() {
+    this.getAllBreeds();
+
   }
 }
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container {
-	max-width: 960px;
-	margin: 0 auto;
+//////////////////////////////
+.image-grid {
+  justify-content: center;
+  /* Arrange children using flexbox */
+  display: flex;
+  /* ALlow images to display on multiple lines rather than a single line */
+  flex-wrap: wrap;
+  /* Container should take up only 90% of the browser, leave 5% space on each side */
+  max-width: 960px;
+  /* Center container */
+  margin: 0 auto;
 }
-.photos {
-	display: flex;
-	background-color: #000;
-	flex-direction: row;
-	flex-wrap: wrap;
-	justify-content: flex-start;
-	align-content: stretch;
-	padding: 0;
-	max-width: 980px;
-	img {
-		display: block;
-		float: left;
-		flex: 0 0 auto;
-		background-color: #fff;
-	}
-}
-@media screen and (min-width: 1024px) {
-	.photos {
-		img {
-			width: calc(100%/6);
-			height: calc(100%/6);
-		}
-	}
-}
-@media screen and (min-width: 769px) and (max-width: 1024px) {
-	.photos {
-		img {
-			width: calc(100%/4);
-			height: calc(100%/4);
-		}
-	}
-}
-@media screen and (min-width: 481px) and (max-width: 768px) {
-	.photos {
-		img {
-			width: calc(100%/3);
-			height: calc(100%/3);
-		}
-	}
-}
-@media screen and (min-width: 321px) and (max-width: 480px) {
-	.photos {
-		img {
-			width: calc(100%/2);
-			height: calc(100%/2);
-		}
-	}
-}
-@media screen and (max-width: 320px) {
-	.photos {
-		img {
-			width: 100%;
-			height: 100%;
-		}
-	}
+.grid-image {
+  /* images display inline by default and act like text. We want them to act like divs, so display block */
+  display: block;
+  /* our width setting for mobile, one image per line */
+  flex-basis: 100%;
+  /* provide some space between our images */
+  padding: 10px;
+  /* ensure padding doesn't cause images to wrap, it should be within the width rather than in addition */
+  box-sizing: border-box;
 }
 
+/* increase images per line as browser gets larger using media queries
+   this first query makes two images per line at 640px width */
+@media only screen and (min-width: 640px) {
+  .grid-image {
+    flex-basis: 50%;
+  }
+}
+
+/* three images per line when browser width exceeds 960px */
+@media only screen and (min-width: 960px) {
+  .grid-image {
+    flex-basis: 33.333%;
+  }
+}
+
+/* four images per line when browser width exceeds 1280px */
+@media only screen and (min-width: 1280px) {
+  .grid-image {
+    flex-basis: 25%;
+  }
+}
+
+/* five images per line when browser width exceeds 1600px */
+@media only screen and (min-width: 1600px) {
+  .grid-image {
+    flex-basis: 20%;
+  }
+}
+
+/////////////////////////////////////////
+
+
+.viu-button--secondary:hover {
+  background: #000;
+  border-color: #000;
+  color: #fff;
+}
+.viu-button {
+  font-family: europa, sans-serif;
+  background: transparent;
+  border: 1px solid #000;
+  color: #000;
+  align-items: center;
+  cursor: pointer;
+//   display: flex;
+  
+  flex-basis: auto;
+  flex-direction: column;
+  flex-grow: 0;
+  flex-shrink: 0;
+  max-height: 48px;
+  min-height: 48px;
+  justify-content: center;
+  padding: 1px 15px;
+  text-align: center;
+  width: 250px;
+  padding-top: 1em;
+}
 </style>
