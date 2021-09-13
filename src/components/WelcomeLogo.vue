5af49e0713c8f9466080633ea8e128d05f6309d3
tmilost <tmilos94@gmail.com>
2021-09-13 03:20:46 +0200

    add welcome logo

---
+++ src/components/WelcomeLogo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="flex justify-center bg-gray-100 sm:items-center sm:pt-0">
+      <img
+        class="logo"
+        src="../assets/logoWelcome.png"
+        alt="OriginTrail"
+        width="300"
+        height="300px"
+      />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  border: 1px solid #2d2d2d;
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
-    <BreedsGrid msg="Welcome to Your Vue.js + TypeScript App" />
+     <WelcomeLogo/>
+    <BreedsGrid />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import WelcomeLogo from "@/components/WelcomeLogo.vue"; // @ is an alias to /src
 import BreedsGrid from "@/components/BreedsGrid.vue"; // @ is an alias to /src
 
 @Component({
   components: {
+    WelcomeLogo,
     BreedsGrid,
   },
 })
