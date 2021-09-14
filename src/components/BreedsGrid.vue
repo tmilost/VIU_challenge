ee515e5a55664f345546b695c6f8e4629e74227c
tmilost <tmilos94@gmail.com>
2021-09-14 23:15:43 +0200

    add expand overlay

--- src/components/BreedsGrid.vue
+++ src/components/BreedsGrid.vue
@@ -19,15 +19,27 @@
           v-for="(breed, index) in allBreedsFiltered"
           :key="index"
         >
-          <el-image
-            class="centered project-image-bg el-image-component"
-            :alt="breed"
-            lazy
-            :src="allBreedWithRandomImage[breed]"
-            :preview-src-list="[...allBreedWithRandomImage[breed]]"
-          >
-          </el-image>
+          <div class="container">
+            <el-image
+              class="centered project-image-bg el-image-component"
+              :alt="breed"
+              lazy
+              :src="allBreedWithRandomImage[breed]"
+              :preview-src-list="[...allBreedWithRandomImage[breed]]"
+            >
+            </el-image>
 
+            <div class="overlay">
+              <a
+                href="#"
+                :preview-src-list="[...allBreedWithRandomImage[breed]]"
+                class="icon"
+                title="click to see image full size"
+              >
+                <i class="fas fa-expand-arrows-alt"></i>
+              </a>
+            </div>
+          </div>
           <button
             class="
               uppercase
@@ -235,5 +247,54 @@ export default class HelloWorld extends Vue {
   font-size: 14px;
   line-height: 22px;
   text-transform: uppercase;
+}
+
+/* Container needed to position the overlay. Adjust the width as needed */
+.container {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+}
+
+/* Make the image to responsive */
+.image {
+  width: 100%;
+  height: auto;
+}
+
+/* The overlay effect (full height and width) - lays on top of the container and over the image */
+.overlay {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: 0.3s ease;
+  background-color: transparent;
+  pointer-events: none;
+}
+
+/* When you mouse over the container, fade in the overlay icon*/
+.container:hover .overlay {
+  opacity: 1;
+}
+
+/* The icon inside the overlay is positioned in the middle vertically and horizontally */
+.icon {
+  color: white;
+  font-size: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+/* When you move the mouse over the icon, change color */
+.fa-user:hover {
+  color: #eee;
 }
 </style>
