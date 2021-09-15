871140f56b5e10f5bfd7ef5f031e007427c26bb6
tmilost <tmilos94@gmail.com>
2021-09-15 06:42:38 +0200

    remove prop

--- src/components/BreedsGrid.vue
+++ src/components/BreedsGrid.vue
@@ -69,7 +69,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import axios from "axios";
 
 @Component
